df4f011361aa77532273eb9843451adacfc62c8a
GM 52. Office <GM52@OFFICE-GM.local>
2024-05-17 09:51:20 +0500

    add user && crud api

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseUUIDPipe, Post } from '@nestjs/common';
+import { UserService } from '@user/user.service';
+import { CreateUserDto } from '@user/dto/create-user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+    constructor(private readonly userService: UserService) {}
+
+    @Post()
+    createUser(@Body() createUserDto: CreateUserDto) {
+        return this.userService.save(createUserDto);
+    }
+
+    @Get()
+    findAllUsers() {
+        return this.userService.findAll();
+    }
+
+    @Get(':idOrEmail')
+    findOneUser(@Param('idOrEmail') idOrEmail: string) {
+        return this.userService.finOne(idOrEmail);
+    }
+
+    @Delete(':id')
+    deleteUser(@Param('id', ParseUUIDPipe) id: string) {
+        return this.userService.delete(id);
+    }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '@prisma/prisma.service';
+import { User } from '@prisma/client';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private readonly prismaService: PrismaService) {}
+
+    save(user: Partial<User>) {
+        const hashedPassword = this.hashPassword(user.password);
+        return this.prismaService.user.create({
+            data: {
+                email: user.email,
+                password: hashedPassword,
+                roles: ['USER'],
+            },
+        });
+    }
+
+    findAll() {
+        return this.prismaService.user.findMany();
+    }
+
+    finOne(idOrEmail: string) {
+        return this.prismaService.user.findFirst({
+            where: {
+                OR: [{ id: idOrEmail }, { email: idOrEmail }],
+            },
+        });
+    }
+
+    delete(id: string) {
+        return this.prismaService.user.delete({
+            where: { id },
+        });
+    }
+
+    private hashPassword(password: string) {
+        return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+    }
+}
